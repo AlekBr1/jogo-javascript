22472a8ff84a238f78d598228f0393065722051b
AlekBr1 <aleksander.fariassilva@gmail.com>
2023-12-03 21:58:01 -0300

    animação parado feita

--- phaser3-typescript-project-template-master/src/game.ts
+++ phaser3-typescript-project-template-master/src/game.ts
@@ -1,36 +1,44 @@
 import * as Phaser from 'phaser';
+import { craetePlayer, loadSprites } from "./player";
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
+export default class Demo extends Phaser.Scene {
+    player;
+    constructor () {
         super('demo');
     }
 
-    preload ()
-    {
+    preload () {
         this.load.image('', 'assets/');
         this.load.image('', 'assets/');
         this.load.glsl('', 'assets/');
+        loadSprites(this);
     }
 
-    create ()
-    {
+    create () {
      const map = this.make.tilemap({key: "map"});
      const tilesetGlass = map.addTilesetImage("grass", "tiles");
      const tilesetWater = map.addTilesetImage("water", "border");
 
      const ground = map.createLayer('grass', tilesetGlass, 0, 0);
      const water = map.createLayer('water', tilesetWater, 0, 0);
+
+     this.player = craetePlayer(this);
+     
+     this.player.anims.play("player_idle", true);
     }
-}
 
 const config = {
     type: Phaser.AUTO,
     backgroundColor: '#125555',
     width: 800,
     height: 600,
-    scene: Demo
+    scene: Demo,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 0}
+        }
+    }
 };
 
 const game = new Phaser.Game(config);
--- phaser3-typescript-project-template-master/src/player.ts
+++ phaser3-typescript-project-template-master/src/player.ts
@@ -1,19 +1,25 @@
-export const loadSprites = (scene) : void => {
+export const craetePlayer = (scene: Phaser.Scene) => {
+    const player = scene.physics.add.sprite(200, 200, 'player_idle');
+    createAnimations(scene);
+    return player;
+}   
+
+export const loadSprites = (scene: Phaser.Scene) : void => {
     scene.load.spritesheet('player_idle', 'local e a img do perso idle.png',{    
         frameWidth: 83,
-        frameHeigth: 64,
+        frameHeight: 64,
         spacing: 45,
     });
 
     scene.load.spritesheet('player_walk', 'local e a img do perso walk.png',{
         frameWidth: 83,
-        frameHeigth: 64,
+        frameHeight: 64,
         spacing: 45,
     });
 
-    scene.loadspritesheet('player_colect', 'local e a img do perso colect.png',{
+    scene.load.spritesheet('player_colect', 'local e a img do perso colect.png',{
         frameWidth: 83,
-        frameHeigth: 64,
+        frameHeight: 64,
         spacing: 45,
     });
 };
@@ -25,7 +31,7 @@ export const createAnimations = (scene: Phaser.Scene) : void => {
             start: 0,
             end: 2,
          }),
-         frameRate: 8   ,
+         frameRate: 6,
          repeat: -1,
          yoyo: true
     });
@@ -36,7 +42,7 @@ export const createAnimations = (scene: Phaser.Scene) : void => {
             start: 0,
             end: 2,
          }),
-         frameRate: 8,
+         frameRate: 6,
          repeat: -1,
          yoyo: true
     });
@@ -45,10 +51,10 @@ export const createAnimations = (scene: Phaser.Scene) : void => {
         key: 'player_colect',
          frames: scene.anims.generateFrameNames('player_colect', {
             start: 0,
-            end: 7,
+            end: 4,
          }),
-         frameRate: 8,
-         repeat: -1,
-         yoyo: true
+         frameRate: 6,
+         repeat: 0,
+         yoyo: false
     });
 } 
